13258f1e2550c015f543d46b8f965b4fce3dda59
longnd97 <nduylong060697@gmail.com>
2021-10-08 10:21:55 +0700

    review_create_food

---
+++ database/migrations/2021_10_08_015034_create_foods_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('image');
+            $table->time('open_time');
+            $table->time('close_time');
+            $table->string('note')->nullable();
+            $table->integer('price');
+            $table->integer('promotional_price');
+            $table->integer('service_price');
+            $table->string('explain_service_price');
+            $table->unsignedBigInteger('service_price_id');
+            $table->time('prepare_time');
+            $table->string('discount_code')->nullable();
+            $table->string('count_discount_code')->nullable();
+            $table->string('tag');
+            $table->string('views')->nullable();
+            $table->string('orders')->nullable();
+            $table->string('nominations')->nullable();
+            $table->string('discount_food')->nullable();
+            $table->string('restaurant')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FoodController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/create', [FoodController::class, 'create'])->name('foods.create');
+Route::post('/create', [FoodController::class, 'store'])->name('foods.store');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
